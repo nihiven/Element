c0e9669a77d3f7f3e760829bec5fc4866ef20cf8
Justin Behanna <j.behanna@gmail.com>
2017-12-10 22:48:57 -0500

    Add project files.

---
+++ Element/AnimatedSprite.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Element
+{
+    class AnimatedSprite
+    {
+        public Texture2D Texture { get; set; }
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        private int currentFrame;
+        private int totalFrames;
+
+        public AnimatedSprite(Texture2D texture, int rows, int columns)
+        {
+            Texture = texture;
+            Rows = rows;
+            Columns = columns;
+            currentFrame = 0;
+            totalFrames = Rows * Columns;
+        }
+    }
+}
---
+++ Element/ElementGame.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Element
+{
+    /// <summary>
+    /// This will hold all player logic and controls.
+    /// </summary>
+    public class Player
+    {
+        public Texture2D PlayerTexture;
+        public Vector2 Position;
+        public bool Active;
+        public int Health;
+
+        /// <summary>
+        /// Returns player width, assumes width of the player is the height of the player texture.
+        /// </summary>
+        public int Width
+        {
+            get { return PlayerTexture.Width; }
+        }
+
+        /// <summary>
+        /// Returns player height, assumes height of the player is the height of the player texture.
+        /// </summary>
+        public int Height
+        {
+            get { return PlayerTexture.Height; }
+        }
+
+        /// <summary>
+        /// Set initial values
+        /// </summary>
+        public void Initialize(Texture2D texture, Vector2 position)
+        {
+            PlayerTexture = texture;
+            Position = position;
+            Active = true;
+            Health = 100;
+        }
+
+        /// <summary>
+        /// Update the player character and all children actors
+        /// </summary>
+        public void Update()
+        {
+
+        }
+
+        /// <summary>
+        /// Draw the player character and child actors
+        /// </summary>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(PlayerTexture, Position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+        }
+    
+    }
+
+
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class ElementGame : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Player player = new Player();
+
+        public ElementGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+            player.Initialize(Content.Load<Texture2D>("player/player_05"), playerPosition);
+
+            base.Initialize();
+      
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            player.Draw(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
